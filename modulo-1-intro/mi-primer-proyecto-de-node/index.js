9310ffd156ff715db18d91130c5b17d1dcc706b4
Santiago Acevedo <santiago.acevedo.m24@gmail.com>
2022-08-17 23:31:22 -0500

    modulo-1

---
+++ modulo-1-intro/mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,12 @@
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+/* 
+Cillum cillum ipsum cillum sit sunt nulla Lorem sint tempor eiusmod. 
+Elit consectetur amet dolor tempor et elit. 
+Voluptate esse in ad laboris in exercitation ut dolore elit enim ut quis. 
+Elit nisi fugiat eiusmod aliqua tempor esse ipsum commodo.
+*/
+
+// Mensaje de despedida
+console.log("Adiós");
