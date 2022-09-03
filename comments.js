a6f32137f7daffccaf7bd3f802c45e9a8758e640
Santiago Acevedo <santiago.acevedo.m24@gmail.com>
2022-09-03 09:48:23 -0500

    comments

---
+++ comments.js
@@ -0,0 +1,14 @@
+/**
+ * My comment
+ * * Import information is highlighted
+ * ! Deprecated method, do not use
+ * ? Should this method be exposed in the public API?
+ * TODO: Activity
+ * @param myParam This is a param for my method
+ */
+////I do not want to see this comment
+//
+// *
+// !
+// ?
+// TODO
